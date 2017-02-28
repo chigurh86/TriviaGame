e508ef5a2fb3e2a500a03b15b73f214e8a8d4ed0
Antonio Caos Jr <antoniocaosjr.@Antonios-MacBook-Pro.local>
2017-02-28 15:52:52 -0500

    newest change

---
+++ html/assets/javascript/app.js
@@ -0,0 +1,29 @@
+var questions = [
+	question: {"What is the name of the band that plays the horns in the Cantina on Tatooine", 
+	choices:["Chewy and the Chewbaccas","","The Starlight Band", "Sonny Eclipse", "Figrin D'an and The Modal Nodes"], 
+	answer: 3
+},
+{
+	question: "Where does the rancor monster, seen in Return of the Jedi, live?" 
+	choices:["Beneath Jabba the Hutts Palace","Inside of an asteroid","A Cave on Hoth","The Dagobah System"], 
+	answer: 3},
+},
+{
+	question: "What planet do jawas live on?",
+	choices:["Endor","Hoth","Tatooine","Earth"], 
+	answer: 2},
+},
+{
+	question: "How old is Yoda when he dies?",
+	choices:["900 Years Old","Just reborn","60 Years Old","300 Years Old"], 
+	answer: 0},
+},
+{
+	question: "Who killed Jabba the Hutt?",
+	choices:["Han Solo","Princess Leia","R2-D2","Luke Skywalker"], 
+	answer: 1}
+	
+];
+
+console.log(questions.choices[0])
+
